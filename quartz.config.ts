b9c860b3e2a0ddeca642c0af2cc412a90e68147e
LeNowe <noe.duvivier2jeux@gmail.com>
2024-07-10 23:01:04 +0200

    Quartz sync: Jul 10, 2024, 11:00 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -23,7 +23,7 @@ const config: QuartzConfig = {
       cdnCaching: true,
       typography: {
         header: "Wittgenstein",
-        body: Wittgenstein",
+        body: "Wittgenstein",
         code: "Wittgenstein",
       },
       colors: {
